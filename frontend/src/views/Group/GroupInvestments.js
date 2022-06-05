d72fa8dad742b243d9555a8c9423519dff15b186
Ivan Gonzalez <kaosolution8@gmail.com>
2022-06-05 13:40:26 -0400

    fixe

--- frontend/src/views/Group/GroupInvestments.js
+++ frontend/src/views/Group/GroupInvestments.js
@@ -1,44 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { toast } from "react-toastify";
-import moment from "moment";
 import { useSelector, useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
 import { useParams, useNavigate } from "react-router-dom";
 
-import { getGroup, reset, updateGroup } from "../../core/redux/features/groups/groupSlice";
+import { getGroup, reset } from "../../core/redux/features/groups/groupSlice";
 
 import Format from "../../core/formats/Format";
-import DialogForm from "../../core/custom-components/dialog/DialogForm";
-import TextField from "../../core/custom-components/form-elements/TextField";
 import Header from "../../core/custom-components/Header";
 import Table from "../../core/custom-components/table/Table";
 import AddLine from "../../core/custom-components/AddLine";
 import Loading from "../../core/custom-components/Loading";
-import {
-  composeValidators,
-  required,
-} from "../../core/custom-components/validations/InputErrors";
 import { Container, Divider, Paper, Typography } from "@mui/material";
-import TextDatePicker from "../../core/custom-components/form-elements/TextDatePicker/index";
-import DialogConfirmation from "../../core/custom-components/dialog/DialogConfirmation";
 
 const GroupInvestments = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { currentGroup, isLoading, isError, isSuccess, message } = useSelector(
+  const { currentGroup, isLoading, isError, message } = useSelector(
     (state) => state.group
   );
 
   const { id, name } = useParams();
-
-  // const [investments, setInvestments] = useState([]);
-  const [openModal, setOpenModal] = useState(false);
-  const [openConfModal, setOpenConfModal] = useState(false);
-  const [isEdit, setIsEdit] = useState(false);
-  const [toEdit, setToEdit] = useState(null);
-  const [toDelete, setToDelete] = useState(null);
 
   const investmentTableColumns = [
     {
@@ -69,75 +53,6 @@ const GroupInvestments = () => {
 
   const investmentCells = ["name", "description", "deposit", "feedback"];
 
-  const handleCreateAction = () => {
-    setIsEdit(false);
-    setToEdit(null);
-    setOpenModal(true);
-  };
-
-  const handleOnEdit = (data) => {
-    setIsEdit(true);
-    const ToEdit = {
-      ...data,
-      date: moment(data.date).format("yyyy-MM-DD"),
-    };
-    setToEdit(ToEdit);
-    setOpenModal(true);
-  };
-
-  const handleOnDelete = (data) => {
-    setToDelete(data._id);
-    setOpenConfModal(true);
-  };
-
-  const onCloseModal = () => {
-    setOpenModal(false);
-  };
-
-  const onCloseConfModal = () => {
-    setOpenConfModal(false);
-  };
-
-  const submitModal = (data) => {
-    if (isEdit) {
-      //   dispatch(updateAction(data));
-      if (isSuccess && !isError) {
-        toast.success(t("actionUpdated"));
-      }
-    } else {
-      const toSend = { ...data, id };
-      //   dispatch(createAction(toSend));
-      if (isSuccess && !isError) {
-        toast.success(t("actionCreated"));
-      }
-    }
-    setOpenModal(false);
-  };
-
-  const onDeleteModal = () => {
-    setOpenConfModal(false);
-    // dispatch(deleteAction(toDelete));
-    if (isSuccess && !isError) {
-      toast.success(t("actionDeleted"));
-    }
-  };
-
-  const getDeposit = (actions) => {
-    let amount = 0;
-    actions.map((a) => {
-        amount += a.amount;
-      })
-    return amount;
-  };
-
-  const getFeedBack = (actions) => {
-    let feedBack = 0;
-    actions.map((a) => {
-        feedBack += a.feedback;
-      })
-    return feedBack;
-  };
-
   const calculateTotals = (list) => {
     let amount = 0;
     let feedBack = 0;
@@ -155,7 +70,6 @@ const GroupInvestments = () => {
 
   useEffect(() => {
     dispatch(getGroup(id));
-    console.log(currentGroup, 'CG')
     return () => {
       dispatch(reset());
     };
@@ -166,21 +80,6 @@ const GroupInvestments = () => {
       toast.error(message);
     }
   }, [isError, message])
-
-  // useEffect(() => {
-  //   if(currentGroup.length > 0){
-  //     let auxArr = []
-
-  //     currentGroup[0].investments.map((i, index) => {
-  //       i = { ...i, deposit: getDeposit(i.actions), feedback: getFeedBack(i.actions) }
-  //       auxArr.push(i)
-  //     })
-
-  //     setInvestments(auxArr)
-  //     console.log(auxArr, investments, 'Investments5847')
-  //   }
-  // }, currentGroup)
-
 
   if (isLoading || currentGroup.length === 0) {
     return <Loading />;
@@ -207,41 +106,6 @@ const GroupInvestments = () => {
         </Paper>
       </Container>
       <AddLine />
-      <DialogForm
-        title={isEdit ? "EditAction" : "CreateAction"}
-        submitText={isEdit ? "Edit" : "Save"}
-        openModal={openModal}
-        onCloseModal={onCloseModal}
-        onSubmitModal={submitModal}
-        data={toEdit}
-      >
-        <TextField
-          field="amount"
-          label="deposit"
-          type="number"
-          validate={composeValidators(required)}
-        />
-        <TextField
-          field="feedback"
-          label="return"
-          type="number"
-          validate={composeValidators(required)}
-        />
-        <TextDatePicker
-          field="date"
-          label="date"
-          type="date"
-          validate={composeValidators(required)}
-        />
-      </DialogForm>
-      <DialogConfirmation
-        title="DeleteAction"
-        confirmationText="deleteActionConfirmation"
-        valueToShow={toDelete?.deposit ? toDelete.deposit : ""}
-        openModal={openConfModal}
-        onCloseModal={onCloseConfModal}
-        onSubmitModal={onDeleteModal}
-      />
     </>
   );
 };
